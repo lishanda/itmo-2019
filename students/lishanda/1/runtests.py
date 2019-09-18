1c5ff389e2a6a1bbc48336176f4801c158e4d753
sasha9012 <sasha901239@gmail.com>
2019-09-18 18:12:30 +0300

    Formated for flake8

--- students/lishanda/1/runtests.py
+++ students/lishanda/1/runtests.py
@@ -1,65 +1,68 @@
-import os
-import importlib.util
-import glob
-import traceback
+# -*- coding: utf-8 -*-
 
-EXTENSION_NOTATION = ".py"
-PREFIX = "test_"
+"""Framework for automatic test collection and running."""
+
+import os
+from importlib import util
+import glob
+
+EXT = '.py'
+PREFIX = 'test_'
 
 
 def load_module(target_file):
-    file_name = os.path.basename(target_file).replace(EXTENSION_NOTATION, "")
+    """Load module for further testing."""
+    file_name = os.path.basename(target_file).replace(EXT, '')
 
-    if len(file_name) == 0 or (not os.path.exists(target_file)):
+    if not file_name or (not os.path.exists(target_file)):
         return None
 
-    spec = importlib.util.spec_from_file_location(file_name, target_file)
-    module = importlib.util.module_from_spec(spec)
+    spec = util.spec_from_file_location(file_name, target_file)
+    module = util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
 
 
 def get_tests_from_module(module):
+    """Gather tests from loaded module."""
     if module is None:
         return None
-    func_dict = dict(filter(lambda elem: elem[0].startswith(PREFIX), vars(module).items()))
-    return func_dict
+
+    extracted_vars = vars(module).items()  # noqa: WPS421
+    test_list = filter(lambda elem: elem[0].startswith(PREFIX), extracted_vars)
+    return dict(test_list)
 
 
-def get_files_from_path(target_path, mask):
-    return glob.glob(f"{target_path}/{mask}", recursive=True)
+def files_from_path(target_path, mask):
+    """Load files from specified folder."""
+    return glob.glob('{0}/{1}'.format(target_path, mask), recursive=True)
 
 
-def launch_testing(target_path="", verbose=True):
-    # Activate verbose flag for traceback logging
-
+def launch_testing(path=''):
+    """Load module for further testing."""
     # If target path is empty current directory is used
-    if len(target_path) == 0:
-        target_path = os.getcwd()
+    if not path:
+        path = os.getcwd()
 
-    # Finding testing modules
-    test_files = get_files_from_path(target_path, f"{PREFIX}*{EXTENSION_NOTATION}")
-
-    for test_file in test_files:
-        module = load_module(test_file)
-        if module is None:
+    for t_file in files_from_path(path, '{0}*{1}'.format(PREFIX, EXT)):
+        if load_module(t_file) is None:
             continue
 
         # Running tests and logging results
-        tests = get_tests_from_module(module)
+        tests = get_tests_from_module(load_module(t_file))
         for test in tests.items():
+            succeed = True
             try:
-                tb = ""
                 test[1]()
-                test_result = "ok"
             except AssertionError:
-                test_result = "fail"
-                tb = traceback.format_exc()
-            print(f"{test_file} | {test[0]} - {test_result}")
-            if verbose and len(tb) != 0:
-                print(tb)
+                succeed = False
+            print('{0} | {1} - {2}'.format(  # noqa: T001
+                t_file,
+                test[0],
+                'ok' if succeed else 'fail',
+            ))
 
 
 if __name__ == '__main__':
-    path = input("Enter path to directory with testing modules\n*Leave blank to choose current directory*: ")
-    launch_testing(path, verbose=False)
+    path = input()  # noqa: WPS421, S322
+    launch_testing(path)
--- students/lishanda/1/test_runtests.py
+++ students/lishanda/1/test_runtests.py
@@ -1,23 +1,31 @@
-# This is module for self-testing of runtests framework
+# -*- coding: utf-8 -*-
 
+"""Module for self-testing of runtests framework."""
 
 import os
 import runtests
 
 
 def test_load_module():
-    assert runtests.load_module(os.getcwd() + "/dont_test_me.py") is not None
-    assert runtests.load_module("") is None
-    assert runtests.load_module("filename.py") is None
+    """Test load_module function."""
+    assert runtests.load_module('{0}/dont_test_me.py'.format(
+        os.getcwd(),
+    )) is None
+    assert runtests.load_module('') is None
+    assert runtests.load_module('filename.py') is None
 
 
 def test_get_tests_from_module():
-    module = runtests.load_module(os.getcwd() + "/test_simple_operations.py")
+    """Test get_tests_from_module function."""
+    module = runtests.load_module('{0}/test_simple_operations.py'.format(
+        os.getcwd(),
+    ))
     assert runtests.get_tests_from_module(module) is not None
     assert runtests.get_tests_from_module(None) is None
 
 
-def test_get_files_from_path():
-    assert len(runtests.get_files_from_path(os.getcwd(), "*.py")) > 0
-    assert len(runtests.get_files_from_path(os.getcwd(), "*")) > 0
-    assert runtests.get_files_from_path(os.getcwd(), "") is not None
+def test_files_from_path():
+    """Test files_from_path function."""
+    assert runtests.files_from_path(os.getcwd(), '*.py')
+    assert runtests.files_from_path(os.getcwd(), '*')
+    assert runtests.files_from_path(os.getcwd(), '') is not None
